e5af58ad6298c9b49c44420c3c30f5045e599d11
Linh Tran <trandinhlinh94@gmail.com>
2017-08-15 11:37:14 +0300

    SpriteKit simple game

---
+++ SKSimpleGame/SKSimpleGame/SKSimpleGame/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  SKSimpleGame
+//
+//  Created by Linh Tran on 14/08/2017.
+//  Copyright © 2017 Linh Tran. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+           }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
